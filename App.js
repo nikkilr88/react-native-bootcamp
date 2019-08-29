7adef7bb00aa68f5522d8ceffec0ca35bb01b967
Nicole Sattler <nicole.sattler@live.com>
2019-08-29 14:15:08 -0500

    Add state and flat list

--- App.js
+++ App.js
@@ -1,26 +1,79 @@
-import React from 'react'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Button,
+  FlatList
+} from 'react-native'
 
-export default function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [goals, setGoals] = useState([
+    { key: Math.random().toString(), value: 'Learn React Native' }
+  ])
+
+  const handleTextChange = textValue => {
+    setInputValue(textValue)
+  }
+
+  const handleAddGoal = () => {
+    setGoals(prevGoals => [
+      ...prevGoals,
+      { key: Math.random().toString(), value: inputValue }
+    ])
+    setInputValue('')
+  }
+
   return (
-    <View style={styles.inputWrapper}>
-      <TextInput style={styles.input} placeholder="What's your goal?" />
-      <Button title="Add" />
+    <View style={styles.view}>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          value={inputValue}
+          style={styles.input}
+          placeholder="What's your goal?"
+          onChangeText={handleTextChange}
+        />
+        <Button title="Add" onPress={handleAddGoal} />
+      </View>
+      <FlatList
+        data={goals}
+        renderItem={goal => (
+          <View style={styles.listItem}>
+            <Text>{goal.item.value}</Text>
+          </View>
+        )}
+      ></FlatList>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  view: {
+    paddingVertical: 50,
+    paddingHorizontal: 25,
+    backgroundColor: 'whitesmoke',
+    flex: 1
+  },
   inputWrapper: {
-    padding: 30,
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 25
   },
   input: {
     borderBottomWidth: 1,
     borderBottomColor: 'blue',
     flex: 1
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: 'white',
+    marginVertical: 5
   }
 })
+
+export default App
